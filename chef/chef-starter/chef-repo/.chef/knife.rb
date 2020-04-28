2de1d43cf0a3968d3435413263f56fdc73caca2c
suresh <suriuma554@gmail.com>
2020-04-28 22:21:20 +0530

    second commit has been done

--- chef/chef-starter/chef-repo/.chef/knife.rb
+++
@@ -1,9 +0,0 @@
-# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
-
-current_dir = File.dirname(__FILE__)
-log_level                :info
-log_location             STDOUT
-node_name                "suriuma554"
-client_key               "#{current_dir}/suriuma554.pem"
-chef_server_url          "https://api.chef.io/organizations/bharat_it"
-cookbook_path            ["#{current_dir}/../cookbooks"]
